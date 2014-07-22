846701b1db58364d506c75b9c1a2dafaec86709c
David Alvarado <davidealva@gmail.com>
2014-07-21 23:44:43 -0600

    finish basic authorize

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,8 +3,8 @@ class Track < ActiveRecord::Base
 
   has_attached_file :audio
   has_attached_file :avatar, 
-                    :styles => { :medium => "200x200>", :thumb => "120x120>" }, 
-                    :default_url => "https://s3-us-west-2.amazonaws.com/trackfilter/default/default_250.png"
+                    :styles => { :medium => "200x200#", :thumb => "120x120#" }, 
+                    :default_url => "/default/default_:style.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
