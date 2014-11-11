83920f12f7525297973873e7d88b72b15fa6d2f3
David Alvarado <davidealva@gmail.com>
2014-11-11 15:13:32 -0700

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,9 +40,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-          :bucket => "Music-Cloud-Dev",
-          :access_key_id => "AKIAJ665XSLEJE3KCMKQ",
-          :secret_access_key => "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"
+          :bucket => ENV["AWS_BUCKET_NAME"],
+          :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+          :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
       }
   }
 end
